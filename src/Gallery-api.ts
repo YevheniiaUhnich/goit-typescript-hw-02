a8c5203bbbaff0b79cf87549ac7015c313d901e6
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-05-19 17:38:55 +0300

    next error

--- src/Gallery-api.ts
+++ src/Gallery-api.ts
@@ -1,16 +1,27 @@
 import axios from "axios";
+import { ImageData } from "./types/ImageData";
 
 const apiKey = "Ud4YJgNphxLzVWMXKSbOYiBwSTEIqFbfAwdnpOzV7M4";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
 
-export const fetchGalleryWithPhoto = async (query, page, signal) => {
+interface FetchGalleryResponse {
+  query: ImageData[];
+  totalPages: number;
+}
+
+export const fetchGalleryWithPhoto = async (
+  query: string,
+  page: number,
+  signal: AbortSignal
+): Promise<FetchGalleryResponse> => {
   const response = await axios.get(
-    `https://api.unsplash.com/search/photos/?client_id=${apiKey}&per_page=20&query=${query}&page=${page}`,
+    `/search/photos/?client_id=${apiKey}&per_page=20&query=${query}&page=${page}`,
     { signal }
   );
+
   return {
-    query: response.data.results,
+    query: response.data.results as ImageData[],
     totalPages: response.data.total_pages,
   };
 };
